6c8d902a6c57dc825b69e27af7f687c8c9984239
mncnf <mncnf0113@gmail.com>
2025-05-22 08:11:43 +0900

    Initial Commit

---
+++ UIKitImagePicker/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  UIKitImagePicker
+//
+//  Created by Development on 2025/05/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ UIKitImagePicker/UIKitImagePickerApp.swift
@@ -0,0 +1,17 @@
+//
+//  UIKitImagePickerApp.swift
+//  UIKitImagePicker
+//
+//  Created by Development on 2025/05/22.
+//
+
+import SwiftUI
+
+@main
+struct UIKitImagePickerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
