4182597a64dbc1b0bf749708db07bddd81863821
mncnf <mncnf0113@gmail.com>
2025-05-22 08:18:37 +0900

    135

--- UIKitImagePicker/ImagePicker.swift
+++ UIKitImagePicker/ImagePicker.swift
@@ -5,6 +5,14 @@
 //  Created by Development on 2025/05/22.
 //
 
+import PhotosUI
 import SwiftUI
 
-struct ImagePicker: UIViewControllerRepresentable {}
+struct ImagePicker: UIViewControllerRepresentable {
+    func makeUIViewController(context: Context) -> PHPickerViewController {
+        var configuration = PHPickerConfiguration()
+        configuration.filter = .images
+        let picker = PHPickerViewController(configuration: configuration)
+        return picker
+    }
+}
